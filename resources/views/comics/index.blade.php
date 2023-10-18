456e29ede4212e729bec714c7ddb60888d0b1765
CarloCassini <c.cassini87@gmail.com>
2023-10-18 16:57:48 +0200

    mostrare la tabella comics.index

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,10 +3,38 @@
 @section('main-content')
   <section class="container mt-5">
     <h1>eccolo</h1>
+
+{{-- intestazione della tabella --}}
+    <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">title</th>
+            <th scope="col">description</th>
+            {{-- <th scope="col">thumb</th> --}}
+            <th scope="col">price</th>
+            <th scope="col">series</th>
+            <th scope="col">sale_date</th>
+            <th scope="col">type</th>
+          </tr>
+        </thead>
+        <tbody>
+
+{{-- caricare i dati nella tabella --}}
     @foreach ($comics as $comic)
-        
-    {{$comic->title}}
-    <br>
-    @endforeach
+    <tr>
+        <th scope="row">{{$comic->id}}</th>
+        <td>{{$comic->title}}</td>
+        <td>{{$comic->description}}</td>
+        {{-- <td>{{$comic->thumb}}</td> --}}
+        <td class="text-end">{{ $comic->price . '€'}}</td>
+        <td>{{$comic->series}}</td>
+        <td>{{$comic->sale_date}}</td>
+        <td>{{$comic->type}}</td>
+      </tr>
+      @endforeach
+    </tbody>
+  </table>
+  
   </section>
 @endsection
