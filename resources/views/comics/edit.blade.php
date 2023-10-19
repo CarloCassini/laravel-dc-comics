0360155e5e56d2252443ed978130599bacaedae2
CarloCassini <c.cassini87@gmail.com>
2023-10-19 15:57:28 +0200

    add page edit - give the comic selected

---
+++ resources/views/comics/edit.blade.php
@@ -0,0 +1,37 @@
+@extends('layouts.app')
+
+@section('main-content')
+  <section class="container mt-5">
+    <h1>eccolo edit</h1>
+    <a href="{{route('comics.index')}}" class="btn btn-primary"> torna alla lista</a>
+
+    <form action="{{route('comics.store')}}" method="POST">
+        {{-- questo token va inserito all'inizio del form per permetterne l'utilizzo --}}
+        @csrf
+    {{-- inserisco i campi da riempire per il caricare il documento --}}
+    <div class="input-group input-group-sm my-3">
+        <span class="input-group-text" id="title">title</span>
+        <input type="text" class="form-control" aria-describedby="title" name="title">
+      </div>
+  
+      <div class="input-group my-3">
+        <span class="input-group-text" id="description">description</span>
+        <textarea class="form-control" aria-describedby="description" name="description"></textarea>
+      </div>
+ 
+      <div class="input-group input-group-sm my-3">
+        <span class="input-group-text" id="price">price</span>
+        <input type="number" class="form-control" aria-describedby="price" name="price">
+      </div>
+   
+      <div class="input-group input-group-sm my-3">
+        <span class="input-group-text" id="series">series</span>
+        <input type="text" class="form-control" aria-describedby="series" name="series">
+      </div>
+
+      <button class="btn btn-primary"> salva
+
+      </button>
+    </form>
+  </section>
+@endsection
--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,6 +3,11 @@
 @section('main-content')
   <section class="container mt-5">
     <h1>eccolo</h1>
+    
+    {{-- il bottone per la lista appare solo se non siamo in --}}
+    @if (Route::currentRouteName() != 'comics.index' )    
+        <a href="{{route('comics.index')}}" class="btn btn-primary"> torna alla lista</a>
+    @endif
 
 {{-- intestazione della tabella --}}
     <table class="table">
@@ -31,11 +36,19 @@
         <td>{{$comic->series}}</td>
         <td>{{$comic->sale_date}}</td>
         <td>{{$comic->type}}</td>
-        <td style="background-color: blue">
-            <a href="{{ route('comics.show' , $comic->id)}}">
-                VAI
+        <td >
+          <div class="d-flex flex-column button-change-db">
+            <a href="{{ route('comics.show' , $comic->id)}}" style="background-color: blue" class="px-5 py-2">
+                dettaglio
             </a>
-            </td>
+            <a href="{{ route('comics.edit' , $comic)}}" style="background-color: green " class="px-5 py-2">
+               modifica
+            </a>
+            <a href="{{ route('comics.show' , $comic->id)}}" style="background-color: red" class="px-5 py-2">
+               cancella
+            </a>
+          </div>
+        </td>
       </tr>
       @endforeach
     </tbody>
